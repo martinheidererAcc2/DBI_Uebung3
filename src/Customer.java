24984f4173c7b06bb616e367c87b20782b07e675
Heiderer2 <m.heiderer@gmx.net>
2020-04-19 14:18:40 +0200

    Class Customer + Customers

---
+++ src/Customer.java
@@ -0,0 +1,40 @@
+
+public class Customer
+{
+	private int customerID;
+	private String firstName;
+	private double credit;
+	
+	public Customer(int customerID, String firstName, double credit)
+	{
+		setCustomerID(customerID);
+		setFirstName(firstName);
+		setCredit(credit);
+	}
+	
+	public int getCustomerID()
+	{
+		return customerID;
+	}
+	public String getFirstName()
+	{
+		return firstName;
+	}
+	public double getCredit()
+	{
+		return credit;
+	}
+	public void setCustomerID(int customerID)
+	{
+		this.customerID = customerID;
+	}
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+	public void setCredit(double credit)
+	{
+		this.credit = credit;
+	}
+	
+}
---
+++ src/Customers.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Customers
+{
+	ArrayList<Customer> customers;
+	
+	public Customers()
+	{
+		customers = new ArrayList<Customer>();
+	}
+	
+}
